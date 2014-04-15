69f27daf96ae800b09afb3e3bde15f3b83957e08
crowdingdev <lundevallan@Linuss-MacBook-Pro.local>
2014-04-15 10:03:23 +0200

    fix

--- opsworks_custom_env/recipes/configure.rb
+++ opsworks_custom_env/recipes/configure.rb
@@ -3,4 +3,13 @@ if node[:opsworks][:instance][:layers].include?('rails-app')
   include_recipe "opsworks_custom_env::restart_command"
   include_recipe "opsworks_custom_env::write_config"
 
+execute 'rake assets:precompile' do
+  cwd "#{node[:deploy_to]}/current"
+  user 'root'
+  command 'bundle exec rake assets:precompile'
+  environment 'RAILS_ENV' => node[:environment_variables][:RAILS_ENV]
+end
+
+
+
 end
